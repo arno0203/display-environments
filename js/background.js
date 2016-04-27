433bb63228d8c5c22c11a046c22f5236705274ae
Arnaud Dollois <arnaud.dollois@voisinsvigilants.org>
2016-04-27 14:05:13 +0200

    wip interaction with browser

--- js/background.js
+++ js/background.js
@@ -1,32 +1,44 @@
-$(document).ready(function(){
-    //setInterval(function () {alert('Page d\'arrière plan en marche');},10000);
-  var config = {dev : [], test:[], recette:[], production:[]}
+$(document).ready(function ()
+{
+  //setInterval(function () {alert('Page d\'arrière plan en marche');},10000);
+  var config = {dev: [], test: [], recette: [], production: []}
 
   localStorage.setItem("config", JSON.stringify(config));
   console.log(config);
   var toto = localStorage.getItem("config");
   console.log(toto);
-});
-$('#click').click(function(){
-    //chrome.tabs.create({url : 'https://youtube.com'})
-    var valeur=JSON.parse(localStorage.getItem("enable"));  //récupérer la valeur
-    var cpt = 0;
-    if(valeur){
-        cpt = parseInt(valeur);
-    }
-    $('#cpt').html(cpt);
 
-    localStorage.setItem("enable",cpt+1);  //donner une nouvelle valeur
+  $('#newBtn').click(function (fenetre, tab)
+  {
+    console.log('newTab');
+  });
 
-    console.log(cpt);
-    console.log(cpt+1);
-    console.log(JSON.parse(localStorage.getItem("config")));
+});
+$('#click').click(function ()
+{
+  //chrome.tabs.create({url : 'https://youtube.com'})
+  var valeur = JSON.parse(localStorage.getItem("enable"));  //récupérer la valeur
+  var cpt = 0;
+  if (valeur)
+  {
+    cpt = parseInt(valeur);
+  }
+  $('#cpt').html(cpt);
+
+  localStorage.setItem("enable", cpt + 1);  //donner une nouvelle valeur
+
+  console.log(cpt);
+  console.log(cpt + 1);
+  console.log(JSON.parse(localStorage.getItem("config")));
+
 });
 
-var notif = new Notification( 'Ma notif',{
-    icon : "img/icon_128.png",
-    body : "Ma notif"
+var notif = new Notification('Ma notif', {
+  icon: "img/icon_128.png",
+  body: "Ma notif"
 });
+
+
 
 
 
